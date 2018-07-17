efc582f5033c65b6f5926fe818bbb9407f4d7716
475402366@qq.com <475402366@qq.com>
2018-07-18 00:04:27 +0800

    #

---
+++ dubbo-client-api/src/main/java/com/integration/boot/provider/service/IUserProviderService.java
@@ -0,0 +1,11 @@
+package com.integration.boot.provider.service;
+
+import java.util.List;
+
+public interface IUserProviderService {
+	
+	
+	@SuppressWarnings("rawtypes")
+	public List getAllUser(String username);
+
+}
